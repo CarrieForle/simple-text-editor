0fc49bc70d6dce50494f23100b8ec80bc48438a6
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-12-12 14:12:58 +0800

    add alternative CutCommand implementation

---
+++ src/CutCommand2.java
@@ -0,0 +1,17 @@
+public class CutCommand2 extends Command {
+    private CopyCommand copyCommand;
+
+    public CutCommand2(Editor editor) {
+        super(editor);
+        this.copyCommand = new CopyCommand(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        this.copyCommand.execute();
+        this.editor.replaceSelected("");
+        this.editor.setStatus("Cut");
+
+        return true;
+    }
+}
